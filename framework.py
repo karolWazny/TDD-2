8a8a2ec6c1d9ba47af66f280b8a91c4aa90c5798
karolWazny <karoljanwazny@gmail.com>
2021-09-07 11:51:42 +0200

    rozdział 20

--- framework.py
+++ framework.py
@@ -9,35 +9,41 @@ class TestCase:
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
+
+    def tearDown(self):
+        pass
 
 
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         self.wasSetUp = None
+        self.log = None
         TestCase.__init__(self, name)
 
     def testMethod(self):
         self.wasRun = 1
+        self.log = self.log + "testMethod "
 
     def setUp(self):
         self.wasSetUp = 1
+        self.log = "setUp "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 
 class TestCaseTest(TestCase):
 
-    def testRunning(self):
-        self.test.run()
-        assert self.test.wasRun
-
-    def testSetUp(self):
-        self.test.run()
-        assert self.test.wasSetUp
-
-    def setUp(self):
-        self.test = WasRun("testMethod")
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
 
 
 if __name__ == '__main__':
-    TestCaseTest("testRunning").run()
-    TestCaseTest("testSetUp").run()
+    testNames = dir(TestCaseTest)
+    for testName in testNames:
+        if testName.startswith("test"):
+            TestCaseTest(testName).run()
