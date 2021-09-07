7e441578d0266bae69b84d9a9bef632145aa2df3
karolWazny <karoljanwazny@gmail.com>
2021-09-07 13:09:23 +0200

    rozdział 23

--- framework.py
+++ framework.py
@@ -5,20 +5,57 @@ class TestCase:
     def setUp(self):
         pass
 
-    def run(self):
-        result = TestResult()
+    def run(self, result):
         result.testStarted()
         try:
             self.setUp()
             method = getattr(self, self.name)
             method()
         except:
-            result.testFailed()
+            result.testFailed(self.name)
         self.tearDown()
         return result
 
     def tearDown(self):
         pass
+
+
+class TestSuite:
+    def __init__(self, testCaseClass=TestCase):
+        self.tests = []
+        testNames = dir(testCaseClass)
+        for testName in testNames:
+            if testName.startswith("test"):
+                self.add(testCaseClass(testName))
+
+    def add(self, test):
+        self.tests.append(test)
+
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
+        return result
+
+
+class TestResult:
+
+    def __init__(self):
+        self.runCount = 0
+        self.errorCount = 0
+        self.failedTestNames = []
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def testFailed(self, testName):
+        self.errorCount += 1
+        self.failedTestNames.append(testName)
+
+    def failedTests(self):
+        return self.failedTestNames
+
+    def summary(self):
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 
 class WasRun(TestCase):
@@ -51,51 +88,50 @@ class SetUpProblems(WasRun):
 
 class TestCaseTest(TestCase):
 
+    def setUp(self):
+        self.result = TestResult()
+
     def testTemplateMethod(self):
         test = WasRun("testMethod")
-        test.run()
+        test.run(TestResult())
         assert ("setUp testMethod tearDown " == test.log)
 
     def testResult(self):
         test = WasRun("testMethod")
-        result = test.run()
-        assert ("1 run, 0 failed" == result.summary())
+        test.run(self.result)
+        assert ("1 run, 0 failed" == self.result.summary())
 
     def testFailedResultFormatting(self):
-        result = TestResult()
-        result.testStarted()
-        result.testFailed()
-        assert ("1 run, 1 failed" == result.summary())
+        self.result.testStarted()
+        self.result.testFailed("testBrokenMethod")
+        assert ("1 run, 1 failed" == self.result.summary())
 
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
-        result = test.run()
-        assert ("1 run, 1 failed" == result.summary())
+        test.run(self.result)
+        assert ("1 run, 1 failed" == self.result.summary())
 
     def testSetUpProblem(self):
         test = SetUpProblems("testMethod")
-        result = test.run()
-        assert ("1 run, 1 failed" == result.summary())
+        test.run(self.result)
+        assert ("1 run, 1 failed" == self.result.summary())
 
+    def testSuite(self):
+        suite = TestSuite(WasRun)
+        suite.run(self.result)
+        assert ("2 run, 1 failed" == self.result.summary())
 
-class TestResult:
-
-    def __init__(self):
-        self.runCount = 0
-        self.errorCount = 0
-
-    def testStarted(self):
-        self.runCount = self.runCount + 1
-
-    def testFailed(self):
-        self.errorCount += 1
-
-    def summary(self):
-        return "%d run, %d failed" % (self.runCount, self.errorCount)
+    def testResultKnowsNamesOfFailedTests(self):
+        suite = TestSuite(WasRun)
+        suite.run(self.result)
+        assert (["testBrokenMethod"] == self.result.failedTests())
 
 
 if __name__ == '__main__':
-    testNames = dir(TestCaseTest)
-    for testName in testNames:
-        if testName.startswith("test"):
-            print(TestCaseTest(testName).run().summary() + "\t" + testName)
+    suite = TestSuite(testCaseClass=TestCaseTest)
+    result = TestResult()
+    suite.run(result)
+    print(result.summary())
+    if len(result.failedTests()) > 0:
+        print("Failed tests:")
+        print(result.failedTests())
